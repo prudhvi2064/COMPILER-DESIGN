a0c602ad8d371ca2680bf3dd6a0f8e9e5ac4ba87
prudhvi2064 <113425657+prudhvi2064@users.noreply.github.com>
2023-01-30 11:32:13 +0530

    Add files via upload

--- lex.yy.c
+++ lex.yy.c
@@ -282,12 +282,11 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[12] =
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
+static yyconst short int yy_accept[8] =
     {   0,
-        0,    0,    5,    4,    3,    2,    1,    0,    1,    1,
-        0
+        0,    0,    4,    2,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -296,8 +295,8 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    3,    1,    3,    4,    3,    5,    5,    5,
-        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,33 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[6] =
+static yyconst int yy_meta[4] =
     {   0,
-        1,    1,    1,    1,    2
+        1,    1,    2
     } ;
 
-static yyconst short int yy_base[13] =
+static yyconst short int yy_base[9] =
     {   0,
-        0,    0,    9,   10,   10,   10,    2,    0,    0,    0,
-       10,    6
+        0,    0,    5,    6,    0,    0,    6,    2
     } ;
 
-static yyconst short int yy_def[13] =
+static yyconst short int yy_def[9] =
     {   0,
-       11,    1,   11,   11,   11,   11,   11,   12,    7,   12,
-        0,   11
+        7,    1,    7,    7,    8,    8,    0,    7
     } ;
 
-static yyconst short int yy_nxt[16] =
+static yyconst short int yy_nxt[10] =
     {   0,
-        4,    5,    6,    4,    7,    8,    9,   10,   11,    3,
-       11,   11,   11,   11,   11
+        4,    4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
-static yyconst short int yy_chk[16] =
+static yyconst short int yy_chk[10] =
     {   0,
-        1,    1,    1,    1,    1,    7,    7,   12,    3,   11,
-       11,   11,   11,   11,   11
+        1,    1,    1,    8,    3,    7,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -362,18 +357,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "calculator.l"
+#line 1 "exp7a.l"
 #define INITIAL 0
-#line 2 "calculator.l"
-
-#undef yywrap
-#define yywrap() 1 
-int f1=0,f2=0;
-char oper;
-float op1=0,op2=0,ans=0;
-void eval();
-
-#line 377 "lex.yy.c"
+#line 3 "exp7a.l"
+int cons=0;
+#line 365 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -524,10 +512,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 16 "calculator.l"
+#line 5 "exp7a.l"
 
-
-#line 531 "lex.yy.c"
+#line 518 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -578,13 +565,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 12 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 10 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -612,57 +599,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 18 "calculator.l"
-{
-	if(f1==0)
-	{
-		op1=atof(yytext);
-		f1=1;
-	}
-
-	else if(f2==-1)
-	{
-		op2=atof(yytext);
-		f2=1;
-	}
-
-	if((f1==1) && (f2==1))
-	{
-		eval();
-		f1=0;
-		f2=0;
-
-	}
-}
+#line 6 "exp7a.l"
+{ cons++; printf("%s is a constant\n", yytext); }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 40 "calculator.l"
-{
-	
-	oper=(char) *yytext;
-	f2=-1;
-}
+#line 7 "exp7a.l"
+{ }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 46 "calculator.l"
-{
-
-	if(f1==1 && f2==1)
-	{	
-		eval;
-		f1=0;
-		f2=0;
-	}
-}	
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 56 "calculator.l"
+#line 8 "exp7a.l"
 ECHO;
 	YY_BREAK
-#line 666 "lex.yy.c"
+#line 616 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -954,7 +904,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 12 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -989,11 +939,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 12 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 11);
+	yy_is_jam = (yy_current_state == 7);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1548,46 +1498,15 @@ int main()
 	return 0;
 	}
 #endif
-#line 56 "calculator.l"
+#line 8 "exp7a.l"
 
-
-
-int main()
+int yywrap(void) { return 1; }
+int main(void)
 {
-	yylex();
-}
-
-
-void eval()
-{
-	switch(oper)
-	{
-		case '+':
-			ans=op1+op2;
-			break;
-
-		case '-':
-			ans=op1-op2;
-			break;
-
-		case '*':
-			ans=op1*op2;
-			break;
-
-		case '/':
-			if(op2==0)
-			{
-				printf("ERROR");
-				return;
-			}
-			else
-			{
-				ans=op1/op2;
-			}
-			break;
-		default:
-			printf("operation not available");
-			break;
-	}
-	printf("The answer is = %lf",ans);
+FILE *f;
+char file[10];
+printf("Enter File Name : "); scanf("%s",file);
+f = fopen(file,"r"); yyin = f;
+yylex();
+printf("Number of Constants : %d\n", cons); fclose(yyin);
 }
